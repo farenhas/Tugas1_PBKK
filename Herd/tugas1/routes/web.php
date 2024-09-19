a583759639ae44af611224a1e58da0444e4073ab
farenhas <faren.haseena@gmail.com>
2024-09-19 14:54:13 +0700

    Model

--- Herd/tugas1/routes/web.php
+++ Herd/tugas1/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
+use App\Models\Post;
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
@@ -18,46 +20,13 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => [
-            [
-                'id' => '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Faren Haseena',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium architecto nisi dignissimos enim rem ipsa iure officia veritatis! Dolor tenetur est ea iure deleniti officiis debitis ut et adipisci aperiam!'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Faren Haseena',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia ut asperiores sapiente fuga blanditiis aliquid nam ab aspernatur? Libero rerum nihil voluptate, esse doloremque alias adipisci repellat recusandae odit quia!'
-            ],
-        ]
-    ]);
+        'posts' => Post::all()]);
+           
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Faren Haseena',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium architecto nisi dignissimos enim rem ipsa iure officia veritatis! Dolor tenetur est ea iure deleniti officiis debitis ut et adipisci aperiam!'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Faren Haseena',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia ut asperiores sapiente fuga blanditiis aliquid nam ab aspernatur? Libero rerum nihil voluptate, esse doloremque alias adipisci repellat recusandae odit quia!'
-        ]
-    ];
-
-    $post = Arr::first($posts, function($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+   
+   $post = Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 
