1d80d8a45e833a97f735cd29f150cba92af418b3
farenhas <faren.haseena@gmail.com>
2024-09-27 11:22:49 +0700

    Post Category

--- Herd/tugas1/routes/web.php
+++ Herd/tugas1/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Faker\Provider\Lorem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -30,6 +32,17 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 
 });
+
+
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->posts) .' Articles by ' .$user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => ' Articles in Category ' .$category->name, 'posts' => $category->posts]);
+});
+
+
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
