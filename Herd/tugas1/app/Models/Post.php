04fe12f28d0e787c6c85dade9850f33cf64051e1
farenhas <faren.haseena@gmail.com>
2024-10-10 01:33:22 +0700

    Pagination

--- Herd/tugas1/app/Models/Post.php
+++ Herd/tugas1/app/Models/Post.php
@@ -2,14 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug','body'];
+
+    protected $with = ['author', 'category'];
 
     public function author(): BelongsTo{
         return $this->belongsTo(User::class);
@@ -18,4 +23,18 @@ class Post extends Model
     public function category(): BelongsTo{
         return $this->belongsTo(Category::class);
     }
+
+    public function scopeFilter(Builder $query, array $filters): void {
+        $query->when($filters['search'] ?? false, fn($query, $search) => 
+            $query->where('title', 'like', '%' . $search . '%')
+        ); 
+
+        $query->when($filters['category'] ?? false, fn($query, $category) => 
+        $query->whereHas('category', fn($query) => $query->where('slug', $category)) // Ganti 'category' dengan $category
+    );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) => 
+        $query->whereHas('author', fn ($query) => $query->where('username', $author)) 
+    );
+}
 }
--- Herd/tugas1/routes/web.php
+++ Herd/tugas1/routes/web.php
@@ -23,23 +23,25 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => Post::all()]);
-           
+        'posts' => \App\Models\Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)->withQueryString()
+    ]);
 });
 
+
 Route::get('/posts/{post:slug}', function (Post $post) {
-   
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
+   return view('post', ['title' => 'Single Post', 'post' => $post]);
 
 });
 
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) .' Articles by ' .$user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' => ' Articles in Category ' .$category->name, 'posts' => $category->posts]);
+    //$posts = $category->posts->load('category', 'author');
+    return view('posts', ['title' => 'Articles in Category ' .$category->name, 'posts' => $category->posts]);
 });
 
 
